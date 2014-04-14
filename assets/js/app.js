8db31aa3f5bbdae368d56b9de97a66e6d80f9f15
urosh <uros.damnjanovic@gmail.com>
2014-04-14 15:21:00 +0300

    Initial setup for the app

---
+++ assets/js/app.js
@@ -0,0 +1,12 @@
+var SearchApp = new Marionette.Application();
+
+SearchApp.addRegions({
+		'searchRegion': '#search-region',
+		'mainRegion': '#main-region',
+		'mapRegion': '#map-region'
+});
+
+SearchApp.on('initialize:after', function(){
+	// 
+})
+
